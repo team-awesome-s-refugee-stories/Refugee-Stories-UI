00195852b394f7c79486bb099cc90c4c4859edae
Spencer Meredith <Smeredith1119@gmail.com>
2019-02-06 08:50:13 -0800

    Added Menu Button worked on styles for the menu button added media query

---
+++ components/Menubutton.js
@@ -0,0 +1,32 @@
+class Menubutton {
+    constructor(element) {
+        // console.log(element);
+
+        this.button = element;
+
+        this.shown = false;
+
+        this.nav = document.querySelector("nav");
+
+        this.button.addEventListener("click", () => {
+            this.toggleContent();
+        });
+    }
+
+    toggleContent() {
+        // Toggle the ".menu-hidden" class off and on
+        if (this.shown) {
+            this.nav.classList.add("menu-hidden");
+            this.nav.classList.remove("menu-shown");
+            this.shown = false;
+        } else {
+            this.nav.classList.remove("menu-hidden");
+            this.nav.classList.add("menu-shown");
+            this.shown = true;
+        }
+    }
+}
+
+// 
+
+let menuButton = new Menubutton(document.querySelector(".menu-button"));
